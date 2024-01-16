7776c5f710633530d0b7c44697c813ef30526f44
xiayulin123 <xiayulinsky@gmail.com>
2024-01-15 20:06:01 -0500

    some questions added

--- firstfile.go
+++ firstfile.go
@@ -8,4 +8,27 @@ func main(){
 	var name string
 	fmt.Scan(&name)
 	fmt.Printf("Hello, %v, welcome to the game \n", name)
+
+	fmt.Printf("Enter your age: ")
+	var age uint
+	fmt.Scan(&age)
+
+	if age >= 10 {
+		fmt.Println("Yay you can play the game!")
+
+	} else {
+		fmt.Println("Sorry you have to be greater than 10 to play this game")
+		return
+	}
+	fmt.Println("continue...")
+	fmt.Printf("Is RTX 3080 better or RTX 3090? ")
+	var answer string
+	var answer2 string
+	fmt.Scan(&answer, &answer2)
+	if answer + " " + answer2 == "RTX 3090"{
+		fmt.Println("Correct!")
+	}else{
+		fmt.Println("False!")
+	}
+
 }
