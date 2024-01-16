70152caa931c06c5c9ffbbbf043b39f29f61023c
xiayulin123 <xiayulinsky@gmail.com>
2024-01-15 19:50:35 -0500

    greetings

--- firstfile.go
+++ firstfile.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main(){
 	fmt.Println("Welcome to the quiz game")
-
-	var name string = "Yulin"
+	fmt.Printf("Enter your name: ")
+	var name string
+	fmt.Scan(&name)
+	fmt.Printf("Hello, %v, welcome to the game \n", name)
 }
